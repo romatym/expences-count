6bd6c784c73929d34af4a56af9166a697ad6182e
romatym <roma_tym@mail.ru>
2020-08-04 17:59:15 +0300

    переделал на async/await

--- src/api/api.js
+++ src/api/api.js
@@ -3,26 +3,20 @@ import queryString from "querystring";
 export const API_URL = "//data.fixer.io/api/latest";
 export const API_KEY = "8d26248b1c5d84fdfef651056a7d1e5a";
 
-export const fetchApi = (url, options = {}) => {
-  return new Promise((resolve, reject) => {
-    fetch(url, options)
-      .then((response) => {
-        if (response.status < 400) {
-          return response.json();
-        } else {
-          throw response;
-        }
-      })
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((response) => {
-        response.json().then((error) => {
-          reject(error);
-        });
-      });
-  });
+export const fetchApi = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+    if (response.status < 400) {
+      return await response.json();
+    } else {
+      throw response;
+    }
+  } catch (err) {
+      return await err.json();
+  }
+
 };
+
 export default class CallApi {
   static get(url, options = {}) {
     const { params = {} } = options;
@@ -37,3 +31,24 @@ export default class CallApi {
     );
   }
 }
+
+// export const fetchApi = (url, options = {}) => {
+//   return new Promise((resolve, reject) => {
+//     fetch(url, options)
+//       .then((response) => {
+//         if (response.status < 400) {
+//           return response.json();
+//         } else {
+//           throw response;
+//         }
+//       })
+//       .then((data) => {
+//         resolve(data);
+//       })
+//       .catch((response) => {
+//         response.json().then((error) => {
+//           reject(error);
+//         });
+//       });
+//   });
+// };
